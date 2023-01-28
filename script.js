57b0c3086b537e8102297386186a71f05b830b62
Ibigelow92 <iobbigelow@gmail.com>
2023-01-28 14:34:25 -0800

    finished hour-block styling, display saved data and clear data

--- script.js
+++ script.js
@@ -3,75 +3,52 @@
 // in the html.
 $(function () {
 
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-
+//Saves our data
 $(".saveBtn").click(function() {
-  var saveBtn = $(this); /*Denotes the parent, which I guess is the save button*/
-  var timeBlock = saveBtn.parent(); /*Denotes the parent of the save buttton, which is the time-block div*/
+  var saveBtn = $(this); /*Denotes the parent, which is the save button*/
+  var timeBlock = saveBtn.parent(); /*Denotes the parent of the save button, which is the time-block div*/
   var timeBlockId = timeBlock.attr("id"); /*Denotes the ID of the time-block*/
   var textArea = saveBtn.siblings("textarea"); /*Selects the textarea*/
   localStorage.setItem(timeBlockId, textArea.val());
 });
 
 function displayToDos() {
-for (var i = 0; i < 9; i ++) {
-  var saveBtn = $(this);
-  var timeBlock = saveBtn.parent();
-  var timeBlockId = timeBlock.attr("id");
-  var textArea = saveBtn.siblings("textarea");
-  var savedData = localStorage.getItem(timeBlockId, textArea.val());
-  textArea.append(savedData);
+for (var i = 9; i <= 17; i ++) { //Using 24-hour format, the day starts at 9 and ends at 17
+  var hour = i.toString()
+  var hourId = "hour-" + hour;
+  $("#"+hourId).children('.event').val(localStorage.getItem(hourId))
 }
 }
 
 displayToDos();
-
+//changes the color of each time block 
 function changeTimeBlockColor() {
 
-  for (var i = 0; i < 9; i++) {
-  var currentHour = dayjs().hour();
-  var hourBlockValue = $('.hour')[i];
+  for (var i = 9; i < 18; i++) {
+  var currentHour = parseInt(dayjs().hour());
+  console.log(currentHour)
+  var hourBlockValue = i;
+  var hourId = "hour-" + i.toString()
 
-    if(hourBlockValue.isBefore(currentHour)) {
-      $('.time-block').addClass('past');
+    if(hourBlockValue<currentHour) {
+      $("#"+hourId).children('.event').addClass('past');
     }
-    if (hourBlockValue.isSame(currentHour)) {
-      $('.time-block').addClass('present');
+    else if (hourBlockValue ===currentHour) {
+      $("#"+hourId).children('.event').addClass('present');
     }
-    if (hourBlockValue.isAfter(currentHour)) {
-      $('.time-block').addClass('future')
+    else  {
+      $("#"+hourId).children('.event').addClass('future')
     }
   }
 }
-
+//calls the function to change the time block
 changeTimeBlockColor();
 
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-
-// loop through all time blocks and do:
-// grab the id like we did above and compare to current hour using day.js api
-// google day.js compare time
-
-
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-
-// loop through all time blocks and do:
-// localStorage.getItem(time block id) -> text to place into the textarea
-
-// TODO: Add code to display the current date in the header of the page.
-
-// use jquery to set text of dat in header to containe current date with current time
+//clears the schedule
+$("#clear-btn").click(function() {
+  localStorage.clear()
+  location.reload()
+});
 });
 
   //Displays today's date
@@ -84,6 +61,6 @@ function displayTodaysDate(now) {
   document.getElementById('currentDay')
     .textContent = now.format('dddd, MMMM D');
 }
-// 
+//
 
 
